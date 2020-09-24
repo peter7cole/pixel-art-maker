122545e63af5ac322924b0a2977ad87ca2cf5b0e
‘peter-robert-cole’ <petercole3@gmail.com>
2020-09-23 20:02:05 -0700

    added react-jss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+	app: {
+		display: 'flex',
+		width: '100%',
+		height: '100%',
+	},
+});
 
 function App() {
+	const classes = useStyles();
 	return (
-		<div>
-			<Grid />
+		<div className={classes.app}>
+			Hello World
+			{/* <Grid /> */}
 		</div>
 	);
 }
