35d2427cde6f9d50967c1bb5bff38abce4af129a
‘peter-robert-cole’ <petercole3@gmail.com>
2020-10-03 17:02:54 -0700

    functional color picker and state

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useStyles from './ColorPicker.styles';
 
+// pass in props, which is lifted state now passed down from App parent
 const ColorPicker = ({ currentColor, setCurrentColor }) => {
 	const classes = useStyles();
 	const colorChange = event => {
