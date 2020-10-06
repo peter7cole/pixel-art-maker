2965aaec76c059acf604093a7ca1f1d5f77ef824
‘peter-robert-cole’ <petercole3@gmail.com>
2020-10-05 17:43:10 -0700

    working on off with click and right click

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -2,19 +2,23 @@ import React, { useState } from 'react';
 import useStyles from './Grid.styles';
 // import { nanoid } from 'nanoid';
 
-const initialCells = Array.from({ length: 40 }, () => ({
+const offCell = {
 	on: false,
 	color: '#ffffff',
-}));
+};
+
+const initialCells = Array.from({ length: 40 }, () => offCell);
 
 const Grid = ({ currentColor }) => {
 	const classes = useStyles();
 	// destructuring!
 	const [cells, setCells] = useState(initialCells);
-	const updateCell = i => () => {
+	const updateCell = (i, defaultState) => e => {
+		e.preventDefault(); // to prevent context menu
 		setCells(
 			cells.map((cell, cellIndex) => {
 				if (cellIndex === i) {
+					if (defaultState) return defaultState;
 					return {
 						on: true,
 						color: currentColor,
@@ -32,6 +36,7 @@ const Grid = ({ currentColor }) => {
 					key={i}
 					style={{ background: cell.on ? cell.color : '#ffffff' }}
 					onClick={updateCell(i)}
+					onContextMenu={updateCell(i, offCell)}
 				></div>
 			))}
 		</div>
