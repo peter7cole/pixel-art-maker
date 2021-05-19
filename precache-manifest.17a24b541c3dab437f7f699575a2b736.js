169fcfde2863decb0e4669ae51898e977d3966d7
‘peter-robert-cole’ <petercole3@gmail.com>
2021-05-18 19:57:14 -0700

    Updates

---
+++ precache-manifest.17a24b541c3dab437f7f699575a2b736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f9495ec2dcecd59367f0c9f67339cf",
+    "url": "/pixel-art-maker/index.html"
+  },
+  {
+    "revision": "28ea08fede2991450269",
+    "url": "/pixel-art-maker/static/css/main.146526bc.chunk.css"
+  },
+  {
+    "revision": "783c5f505d185e7ace80",
+    "url": "/pixel-art-maker/static/js/2.dd2b9f69.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pixel-art-maker/static/js/2.dd2b9f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ea08fede2991450269",
+    "url": "/pixel-art-maker/static/js/main.7b1eb60a.chunk.js"
+  },
+  {
+    "revision": "616e2f8e4af4db7cab99",
+    "url": "/pixel-art-maker/static/js/runtime-main.5f811599.js"
+  }
+]);
